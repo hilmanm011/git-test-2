ef00a9fad46fa3d0c70042df6a6487bb1b54f7b9
Muhamad Hilman <muhamadhilman885@gmail.com>
2019-09-03 00:57:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,8 @@
     <h1>belajar git</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus neque enim, placeat doloremque hic deserunt quidem cupiditate, quasi accusamus earum quaerat adipisci,
      sint quos et numquam eligendi maxime sapiente pariatur?</p>
+
+    <li>satu</li>
+    <li>dua</li>
 </body>
 </html>
